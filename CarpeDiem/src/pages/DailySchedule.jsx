4a00def7b40c6447576014b9451dcb53dbec7c9c
Daniel Friesen <djmartens.friesen@gmail.com>
2026-05-27 14:15:13 -0700

    formatted basic schedule layout

--- CarpeDiem/src/pages/DailySchedule.jsx
+++ CarpeDiem/src/pages/DailySchedule.jsx
@@ -3,54 +3,66 @@ export default function DailySchedule() {
         <div className="body">
             {/* Build out the 3 main panels - tracking dash, task board, and main schedule */}
             <div className="dashboard">
-                <div className="weekly-progress">
+                <div className="weekly-progress card">
                     <h2>Weekly Progress</h2>
                     <p>Graph goes here</p>
                 </div>
-                <div className="roadmap-progress">
+                <div className="roadmap-progress card">
                     <h2>Roadmap Progress</h2>
                     <p>Graph goes here</p>
                 </div>
-                <div className="completion-progress">
+                <div className="completion-progress card">
                     <h2>Completion Progress</h2>
                     <p>Graph goes here</p>
                 </div>
             </div>
             <div className="schedule-section">
-                <div className="template-panel">
+                <div className="template-panel card">
                     <h2>Templates</h2>
                     <div className="template-list">
-                        <ul>Workout</ul>
-                        <ul>Study</ul>
-                        <ul>Build house</ul>
+                        <div className="template-item card">
+                            <p>Template Item 1</p>
+                        </div>
+                        <div className="template-item card">
+                            <p>Template Item 2</p>
+                        </div>
+                        <div className="template-item card">
+                            <p>Template Item 3</p>
+                        </div>
                     </div>
                 </div>
-                <div className="schedule">
+                <div className="schedule card">
                     <h2 className="schedule-title">The Day</h2>
                     <div className="schedule-rows">
-                        <div className="template-blocks">
+                        <div className="schedule-titles schedule-item">
                             <h3>Blocks</h3>
-                            <ul>Item 1</ul>
-                            <ul>Item 2</ul>
-                            <ul>Item 3</ul>
-                            <ul>Item 4</ul>
-                            <ul>Item 5</ul>
-                        </div>
-                        <div className="block-completed">
                             <h3>Completed</h3>
-                            <ul>Item 1</ul>
-                            <ul>Item 2</ul>
-                            <ul>Item 3</ul>
-                            <ul>Item 4</ul>
-                            <ul>Item 5</ul>
-                        </div>
-                        <div className="block-notes">
                             <h3>Notes</h3>
-                            <ul>Item 1</ul>
-                            <ul>Item 2</ul>
-                            <ul>Item 3</ul>
-                            <ul>Item 4</ul>
-                            <ul>Item 5</ul>
+                        </div>
+                        <div className="schedule-item card">
+                            <p>Blocks</p>
+                            <p>Completed</p>
+                            <p>Notes</p>
+                        </div>
+                        <div className="schedule-item card">
+                            <p>Blocks</p>
+                            <p>Completed</p>
+                            <p>Notes</p>
+                        </div>
+                        <div className="schedule-item card">
+                            <p>Blocks</p>
+                            <p>Completed</p>
+                            <p>Notes</p>
+                        </div>
+                        <div className="schedule-item card">
+                            <p>Blocks</p>
+                            <p>Completed</p>
+                            <p>Notes</p>
+                        </div>
+                        <div className="schedule-item card">
+                            <p>Blocks</p>
+                            <p>Completed</p>
+                            <p>Notes</p>
                         </div>
                     </div>
                 </div>
